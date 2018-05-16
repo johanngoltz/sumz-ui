4897053348965c449e95445b11b546a4e27632e5
Johann Goltz <johann-goltz@hotmail.de>
2018-05-16 20:30:30 +0200

    fix linting errors

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -9,21 +9,22 @@ import {
   MatProgressBarModule, MatSlideToggleModule, MatButtonToggleModule, MatProgressSpinnerModule, MatCommonModule
 } from '@angular/material';
 
+
 @NgModule({
   imports: [
     CommonModule,
     MatButtonModule, MatCheckboxModule, MatCardModule, MatMenuModule, MatIconModule, MatLineModule, MatListModule, MatSortModule,
     MatTabsModule, MatTreeModule, MatBadgeModule, MatChipsModule, MatInputModule, MatRadioModule, MatAutocompleteModule, MatTableModule,
-    MatDialogModule, MatOptionModule, MatRippleModule, MatSelectModule, MatSliderModule, MatDividerModule, MatSidenavModule, MatStepperModule,
-    MatToolbarModule, MatTooltipModule, MatGridListModule, MatSnackBarModule, MatFormFieldModule, MatPaginatorModule, MatDatepickerModule,
-    MatProgressBarModule, MatSlideToggleModule, MatButtonToggleModule, MatProgressSpinnerModule, MatCommonModule
+    MatDialogModule, MatOptionModule, MatRippleModule, MatSelectModule, MatSliderModule, MatDividerModule, MatSidenavModule,
+    MatStepperModule, MatToolbarModule, MatTooltipModule, MatGridListModule, MatSnackBarModule, MatFormFieldModule, MatPaginatorModule,
+    MatDatepickerModule, MatProgressBarModule, MatSlideToggleModule, MatButtonToggleModule, MatProgressSpinnerModule, MatCommonModule
   ],
   exports: [
     MatButtonModule, MatCheckboxModule, MatCardModule, MatMenuModule, MatIconModule, MatLineModule, MatListModule, MatSortModule,
     MatTabsModule, MatTreeModule, MatBadgeModule, MatChipsModule, MatInputModule, MatRadioModule, MatAutocompleteModule, MatTableModule,
-    MatDialogModule, MatOptionModule, MatRippleModule, MatSelectModule, MatSliderModule, MatDividerModule, MatSidenavModule, MatStepperModule,
-    MatToolbarModule, MatTooltipModule, MatGridListModule, MatSnackBarModule, MatFormFieldModule, MatPaginatorModule, MatDatepickerModule,
-    MatProgressBarModule, MatSlideToggleModule, MatButtonToggleModule, MatProgressSpinnerModule, MatCommonModule
+    MatDialogModule, MatOptionModule, MatRippleModule, MatSelectModule, MatSliderModule, MatDividerModule, MatSidenavModule,
+    MatStepperModule, MatToolbarModule, MatTooltipModule, MatGridListModule, MatSnackBarModule, MatFormFieldModule, MatPaginatorModule,
+    MatDatepickerModule, MatProgressBarModule, MatSlideToggleModule, MatButtonToggleModule, MatProgressSpinnerModule, MatCommonModule
   ]
 })
 export class MaterialModule { }
