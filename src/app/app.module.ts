8ad9c4667c17c7f31a27842402b47c7938b7ce08
Johann Goltz <j.goltz@sap.com>
2018-06-06 11:49:22 +0200

    activeFilter skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
-import { AppComponent } from './app.component';
-import { MaterialModule } from './material.module';
-import { ProjectsComponent } from './projects/projects.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './/app-routing.module';
+import { ActiveFilterPipe } from './active-filter.pipe';
+import { AppComponent } from './app.component';
 import { CreateProjectComponent } from './create-project/create-project.component';
+import { MaterialModule } from './material.module';
 import { ProjectDetailComponent } from './project-detail/project-detail.component';
+import { ProjectsComponent } from './projects/projects.component';
 import { ScenarioTileComponent } from './scenario-tile/scenario-tile.component';
+
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { ScenarioTileComponent } from './scenario-tile/scenario-tile.component';
     CreateProjectComponent,
     ProjectDetailComponent,
     ScenarioTileComponent,
+    ActiveFilterPipe,
   ],
   imports: [
     BrowserModule,
