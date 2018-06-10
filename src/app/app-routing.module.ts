99655e9ee400825408da5f91699f04d6021bda30
Tobias Pastrzig <tomastepa@web.de>
2018-06-10 12:52:41 +0200

    Revert "Revert "login component created""
    This reverts commit febd5eec1e006b03dc340a2a16a2690590424dc3.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { CreateProjectComponent } from './create-project/create-project.component';
 import { ProjectDetailsComponent } from './project-details/project-details.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: ProjectsComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'create', component: CreateProjectComponent },
   { path: 'project/:id', component: ProjectDetailsComponent },
 ];
