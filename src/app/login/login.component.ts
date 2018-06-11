f0002bc9d36b961ab215a89bee03e60064d6d21f
Tobias Pastrzig <tomastepa@web.de>
2018-06-11 22:24:16 +0200

    error messages added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,12 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
+  loginFormGroup: FormGroup;
   hide = true;
 
-  ngOnInit() {
+  constructor(private _formBuilder: FormBuilder) { // <--- inject FormBuilder
   }
 
+  ngOnInit() {
+    this.loginFormGroup = this._formBuilder.group({
+      mailCtrl: ['', Validators.email],
+      pwdCtrl: '',
+    });
+  }
+
+  get mailCtrl() { return this.loginFormGroup.get('mailCtrl'); }
+
+  get pwdCtrl() { return this.loginFormGroup.get('pwdCtrl'); }
 }
