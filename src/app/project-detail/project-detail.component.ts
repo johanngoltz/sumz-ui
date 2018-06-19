cf5cc4249dfe17e4941f4d08f8da77af5d136c02
Johann Goltz <johann-goltz@hotmail.de>
2018-06-19 14:42:03 +0200

    implement reactive creation of empty scenario

--- src/app/project-detail/project-detail.component.ts
+++ src/app/project-detail/project-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable, of, from } from 'rxjs';
-import { switchMap, merge, map, withLatestFrom, flatMap, filter } from 'rxjs/operators';
+import { Observable, of, from, BehaviorSubject, ReplaySubject, ConnectableObservable, interval } from 'rxjs';
+import { switchMap, merge, map, withLatestFrom, flatMap, filter, publish, tap } from 'rxjs/operators';
 import { FinancialData, Project, Scenario } from '../project';
 import { ProjectsService } from '../projects.service';
 import { ScenariosService } from '../scenarios.service';
@@ -16,24 +16,36 @@ export class ProjectDetailComponent implements OnInit {
   private forProject$: Observable<Project>;
   private allScenarios$: Observable<Scenario[]>;
   private activeScenarios$: Observable<Scenario[]>;
+  private projectId: Observable<number>;
   timeSeriesColumns = ['year', 'externalCapital', 'fcf'];
   scenarioColumns = ['position', 'equityInterest', 'outsideCapitalInterest', 'corporateTax'];
 
-  constructor(private scenariosService: ScenariosService,
+  constructor(private _scenariosService: ScenariosService,
     private projectsService: ProjectsService,
     private route: ActivatedRoute) {
   }
 
   ngOnInit() {
     const projectId$ = this.route.paramMap.pipe(switchMap(params => of(Number.parseFloat(params.get('id')))));
+    this.projectId = projectId$;
     this.forProject$ = projectId$.pipe(
       switchMap(projectId => this.projectsService.getProject(projectId))
     );
     this.allScenarios$ = projectId$.pipe(
-      switchMap(projectId => this.scenariosService.getScenarios(projectId))
+      switchMap(projectId => this._scenariosService.getScenarios(projectId))
     ).pipe(flatMap(a => a));
     this.activeScenarios$ = this.allScenarios$.pipe(
       switchMap(scenarios => of(scenarios.filter(s => s.isActive)))
     );
+  }
+
+  private addScenario() {
+    of({} as Scenario).pipe(
+      withLatestFrom(this.projectId),
+      map(x => {
+        const [scenario, pId] = x;
+        this._scenariosService.addScenario(pId, scenario);
+      })
+    ).subscribe();
   }
 }
