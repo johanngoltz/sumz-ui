8c210d391841343d91e0772e7423f1203d269267
Johann Goltz <j.goltz@sap.com>
2018-06-22 18:41:05 +0200

    remove scenario tile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,19 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ChartsModule } from 'ng2-charts';
 import { AppRoutingModule } from './/app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateProjectComponent } from './create-project/create-project.component';
-import { MaterialModule } from './material.module';
-import { ProjectDetailComponent } from './project-detail/project-detail.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { ToDoubleDirective } from './to-double.directive';
-import { SelectProjectComponent } from './select-project/select-project.component';
-import { ProjectCardComponent } from './project-card/project-card.component';
-import { ScenarioTileComponent } from './scenario-tile/scenario-tile.component';
 import { DeleteDialogComponent } from './delete-dialog/delete-dialog.component';
 import { LoginComponent } from './login/login.component';
+import { MaterialModule } from './material.module';
+import { ProjectCardComponent } from './project-card/project-card.component';
+import { ProjectDetailComponent } from './project-detail/project-detail.component';
+import { ProjectsComponent } from './projects/projects.component';
 import { RegistrationComponent } from './registration/registration.component';
-import { ChartsModule } from 'ng2-charts';
+import { SelectProjectComponent } from './select-project/select-project.component';
+import { ToDoubleDirective } from './to-double.directive';
 
 @NgModule({
   declarations: [
@@ -27,7 +26,6 @@ import { ChartsModule } from 'ng2-charts';
     ToDoubleDirective,
     SelectProjectComponent,
     ProjectCardComponent,
-    ScenarioTileComponent,
     DeleteDialogComponent,
     LoginComponent,
     RegistrationComponent,
