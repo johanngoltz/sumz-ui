de69a5cb70c86f1b84b67cc2c50b30b024ff1de7
Johann Goltz <j.goltz@sap.com>
2018-06-22 19:02:09 +0200

    api definition

---
+++ src/app/api/api.d.ts
@@ -0,0 +1,14 @@
+import { Scenario } from './scenario';
+
+export interface ScenarioAPI {
+    '/scenario': {
+        GET: { response: Scenario },
+        POST: { body: Scenario, response: Scenario }
+    },
+    '/scenario/:sId': {
+        params: { sId: number },
+        GET: { response: Scenario },
+        PUT: { body: Scenario, response: Scenario },
+        DELETE: {}
+    }
+}
