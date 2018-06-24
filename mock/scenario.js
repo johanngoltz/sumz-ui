065e3ce3b9585f2e4f6d98398e90aa3f4afca7c5
Johann Goltz <johann-goltz@hotmail.de>
2018-06-24 20:48:14 +0200

    add delete functionality to mock server

--- mock/scenario.js
+++ mock/scenario.js
@@ -1,35 +1,46 @@
 var utils = require('./utils');
 
 exports.routes = [{
-    route: '/scenario',
-    verb: 'GET',
-    handler: function (req, res) {
-      res.json(state.scenarios);
-    },
+  route: '/scenario',
+  verb: 'GET',
+  handler: function (req, res) {
+    res.json(state.scenarios);
   },
-  {
-    route: '/scenario',
-    verb: 'POST',
-    handler: function (req, res) {
-      state.scenarios.push(req.body)
-      res.json(req.body);
-      res.status(201);
-    },
+},
+{
+  route: '/scenario',
+  verb: 'POST',
+  handler: function (req, res) {
+    state.scenarios.push(req.body)
+    res.json(req.body);
+    res.status(201);
   },
-  {
-    route: '/scenario/:sId',
-    verb: 'GET',
-    handler: function (req, res) {
-      var scenario = utils.getById(req.params.sId, state.scenarios);
-      res.json(scenario);
-    },
+},
+{
+  route: '/scenario/:sId',
+  verb: 'GET',
+  handler: function (req, res) {
+    var scenario = utils.getById(req.params.sId, state.scenarios);
+    res.json(scenario);
   },
-  {
-    route: '/scenario/:sId',
-    verb: 'PUT',
-    handler: function (req, res) {
-      utils.setById(req.params.sId, state.scenarios, req.body);
-      res.json(req.body);
-    }
+},
+{
+  route: '/scenario/:sId',
+  verb: 'PUT',
+  handler: function (req, res) {
+    utils.setById(req.params.sId, state.scenarios, req.body);
+    res.json(req.body);
   }
+},
+{
+  route: '/scenario/:sId',
+  verb: 'DELETE',
+  handler: function (req, res) {
+    state.scenarios.splice(
+      state.scenarios.findIndex(function (scenario) {
+        return scenario.id === req.params.sId;
+      }),
+      1);
+  }
+}
 ];
