2eca9d2fa3e370f798857ae7ca32d585235bea11
Johann Goltz <j.goltz@sap.com>
2018-06-25 10:33:10 +0200

    "fix" unresolvable dependeny

--- src/app/scenario-card/scenario-card.component.ts
+++ src/app/scenario-card/scenario-card.component.ts
@@ -9,8 +9,15 @@ import { ScenariosService } from '../service/scenarios.service';
   templateUrl: './scenario-card.component.html',
   styleUrls: ['./scenario-card.component.css'],
 })
+
+
 export class ScenarioCardComponent implements OnInit {
-  @Input() scenario: Scenario;
+  // Schlimme Sache.
+  // Wird scenario direkt auf Scenario typsiert, kommt es beim Compilieren zu einem Fehler, weil die
+  // Dependency nicht aufgelöst werden kann. Mit der Wrapper-Klasse gibt es das Problem nicht, dafür muss der Wert
+  // immer mit valueOf() adressiert werden, um nicht statische Typfehler zu bekommen.
+  // Zur Laufzeit hat scenario den Typ Scenario.
+  @Input() scenario: Wrapper<Scenario>;
   @ViewChild(MatMenuTrigger) trigger: MatMenuTrigger;
   hovered: Boolean;
 
@@ -27,16 +34,21 @@ export class ScenarioCardComponent implements OnInit {
   }
 
   removeScenario() {
-    this._dialog.open(DeleteDialogComponent, { data: { scenario: this.scenario } })
+    this._dialog.open(DeleteDialogComponent, { data: { scenario: this.scenario.valueOf() } })
       .afterClosed().subscribe((result) => {
         if (result === true) {
-          this._scenariosService.removeScenario(this.scenario)
+          this._scenariosService.removeScenario(this.scenario.valueOf())
             .subscribe(
-              removed => this._snackBar.open(`Das Szenario "${this.scenario.name}" wurde erfolgreich gelöscht`, undefined,
+              removed => this._snackBar.open(`Das Szenario "${this.scenario.valueOf().name}" wurde erfolgreich gelöscht`, undefined,
                 { duration: 5000 }),
-              error => this._snackBar.open(`Das Projekt "${this.scenario.name}" konnte nicht gelöscht werden (${error.message})`,
+              error => this._snackBar.open(`Das Projekt "${this.scenario.valueOf().name}" konnte nicht gelöscht werden (${error.message})`,
                 undefined, { panelClass: 'mat-warn', duration: 5000 }));
         }
       });
+  }
+}
+class Wrapper<T> extends Object {
+  valueOf(): T {
+    return null;
   }
 }
