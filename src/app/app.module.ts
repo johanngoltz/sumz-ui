d5ce79a3c1e89fcebc540cebb7b213ec0d6d567b
Sta7e <jonas-robl@web.de>
2018-06-29 20:39:45 +0200

    app.module.ts

---
+++ src/app/app.module.ts
@@ -0,0 +1,49 @@
+
+import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './/app-routing.module';
+import { AppComponent } from './app.component';
+import { CreateScenarioComponent } from './create-scenario/create-scenario.component';
+import { DeleteDialogComponent } from './delete-dialog/delete-dialog.component';
+import { LoginComponent } from './login/login.component';
+import { MaterialModule } from './material.module';
+import { RegistrationComponent } from './registration/registration.component';
+import { ScenarioCardComponent } from './scenario-card/scenario-card.component';
+import { ScenarioDetailComponent } from './scenario-detail/scenario-detail.component';
+import { ScenariosComponent } from './scenarios/scenarios.component';
+import { SelectScenarioComponent } from './select-scenario/select-scenario.component';
+import { ToDoubleDirective } from './to-double.directive';
+import { LOCAL_CONFIG, DEV_LOCAL_CONFIG } from './app.config';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ScenariosComponent,
+    CreateScenarioComponent,
+    ScenarioDetailComponent,
+    ToDoubleDirective,
+    SelectScenarioComponent,
+    ScenarioCardComponent,
+    DeleteDialogComponent,
+    LoginComponent,
+    RegistrationComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    NgxChartsModule,
+  ],
+  providers: [{ provide: LOCAL_CONFIG, useValue: DEV_LOCAL_CONFIG }],
+  bootstrap: [AppComponent],
+  entryComponents: [SelectScenarioComponent, DeleteDialogComponent],
+})
+export class AppModule { }
