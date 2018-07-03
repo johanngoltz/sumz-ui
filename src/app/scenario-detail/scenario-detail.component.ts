6260031540be28acd01693c3ef7e827ec6ae089a
Sta7e <jonas-robl@web.de>
2018-07-03 11:11:29 +0200

    alertservice, polishing, mockdata, graph waits vor scenarioservice

--- src/app/scenario-detail/scenario-detail.component.ts
+++ src/app/scenario-detail/scenario-detail.component.ts
@@ -1,16 +1,61 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Observable, of, fromEvent, from, EMPTY } from 'rxjs';
-import { switchMap, withLatestFrom, tap, first } from 'rxjs/operators';
-import { Scenario } from '../api/scenario';
-import { RemoteConfig } from '../api/config';
-import { ScenariosService } from '../service/scenarios.service';
-import { FormControl, FormGroupDirective, NgForm, Validators, FormGroup, FormBuilder } from '@angular/forms';
-import { OptionsService } from '../service/options.service';
-import { ErrorStateMatcher } from '@angular/material/core';
-import { trigger, transition, query, animate, style, keyframes } from '@angular/animations';
-import { paramData } from '../api/paramData';
-import { Chart } from "angular-highcharts";
+import {
+  Component,
+  OnInit,
+  OnDestroy
+} from '@angular/core';
+import {
+  ActivatedRoute
+} from '@angular/router';
+import {
+  Observable,
+  of ,
+  fromEvent,
+  from,
+  EMPTY
+} from 'rxjs';
+import {
+  switchMap,
+  withLatestFrom,
+  tap,
+  first
+} from 'rxjs/operators';
+import {
+  Scenario
+} from '../api/scenario';
+import {
+  RemoteConfig
+} from '../api/config';
+import {
+  ScenariosService
+} from '../service/scenarios.service';
+import {
+  AlertService
+} from '../service/alert.service';
+import {
+  FormControl,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+  FormGroup,
+  FormBuilder
+} from '@angular/forms';
+import {
+  OptionsService
+} from '../service/options.service';
+import {
+  trigger,
+  transition,
+  query,
+  animate,
+  style,
+  keyframes
+} from '@angular/animations';
+import {
+  paramData
+} from '../api/paramData';
+import {
+  Chart
+} from 'angular-highcharts';
 
 @Component({
   selector: 'app-scenario-detail',
@@ -20,30 +65,58 @@ import { Chart } from "angular-highcharts";
     trigger('toggleEditAnimation', [
       transition('* => *', [
         query(':enter', [
-          style({ position: 'absolute', opacity: 0, right: 0 }),
-        ], { optional: true }),
+          style({
+            position: 'absolute',
+            opacity: 0,
+            right: 0,
+          }),
+        ], {
+          optional: true,
+        }),
         query(':leave', [
           animate('.2s cubic-bezier(0.4, 0.0, 1, 1)', keyframes([
-            style({ transform: 'translateY(0px)', opacity: 1 }),
-            style({ transform: 'translateY(15px)', opacity: 0 }),
+            style({
+              transform: 'translateY(0px)',
+              opacity: 1,
+            }),
+            style({
+              transform: 'translateY(15px)',
+              opacity: 0,
+            }),
           ])),
-          style({ position: 'absolute', opacity: 0, right: 0 }),
-        ], { optional: true }),
+          style({
+            position: 'absolute',
+            opacity: 0,
+            right: 0,
+          }),
+        ], {
+          optional: true,
+        }),
         query(':enter', [
-          style({ position: 'static' }),
+          style({
+            position: 'static',
+          }),
           animate('.2s cubic-bezier(0.0, 0.0, 0.2, 1)', keyframes([
-            style({ transform: 'translateY(15px)', opacity: 0 }),
-            style({ transform: 'translateY(0px)', opacity: 1 }),
+            style({
+              transform: 'translateY(15px)',
+              opacity: 0,
+            }),
+            style({
+              transform: 'translateY(0px)',
+              opacity: 1,
+            }),
           ])),
-        ], { optional: true }),
+        ], {
+          optional: true,
+        }),
       ]),
     ]),
   ],
 })
 
 export class ScenarioDetailComponent implements OnInit, OnDestroy {
-  forScenario$: Observable<Scenario>;
-  forConfig$: Observable<RemoteConfig>;
+  forScenario$: Observable < Scenario > ;
+  forConfig$: Observable < RemoteConfig > ;
   formGroup: FormGroup;
   accountingDataFormGroup: FormGroup;
   paramData = paramData;
@@ -64,12 +137,12 @@ export class ScenarioDetailComponent implements OnInit, OnDestroy {
   chart;
 
   constructor(private _scenariosService: ScenariosService, private _formBuilder: FormBuilder, private _optionsService: OptionsService,
-    private route: ActivatedRoute) { }
+    private _alertService: AlertService, private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.editable = false;
     this.forScenario$ = this.route.paramMap.pipe(
-      switchMap(params => of(Number.parseInt(params.get('id')))),
+      switchMap(params => of (Number.parseInt(params.get('id')))),
       switchMap(scenarioId => this._scenariosService.getScenario(scenarioId)));
     this.forConfig$ = this._optionsService.getConfig();
     this.formGroup = this._formBuilder.group({
@@ -87,62 +160,37 @@ export class ScenarioDetailComponent implements OnInit, OnDestroy {
       this.showFcf = config.showResult.fcf;
       this.showFte = config.showResult.fte;
     });
-
-  this.chart = new Chart({
-    chart: {
-      type: 'line',
-    },
-    credits: {
-      enabled: false,
-    },
-    legend: {
-      enabled: false,
-    },
-    yAxis: {
-      title: {text: 'Unternehmenswert in €'},
-    },
-    xAxis: {
-      categories: [
-        '-3 σ',
-        '',
-        '',
-        '',
-        '',
-        '-2 σ',
-        '',
-        '',
-        '',
-        '',
-        '-1 σ',
-        '',
-        '',
-        '',
-        '',
-        'μ',
-        '',
-        '',
-        '',
-        '',
-        '1 σ',
-        '',
-        '',
-        '',
-        '',
-        '2 σ',
-        '',
-        '',
-        '',
-        '',
-        '3 σ',
-      ],
-    },
-    series: [
-      {
-        name: ' ',
-        data: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0],
-      },
-    ],
-  });
+    this.forScenario$.pipe(first()).subscribe(currentScenario => {
+      this.chart = new Chart({
+        chart: {
+          type: 'line',
+        },
+        credits: {
+          enabled: false,
+        },
+        legend: {
+          enabled: false,
+        },
+        title: {
+          text: '',
+        },
+        yAxis: {
+          title: {
+            text: 'Verteilung',
+          },
+        },
+        xAxis: {
+          categories: currentScenario.companyValueDistribution.xValues,
+          title: {
+            text: 'Unternehmenswert in €',
+          },
+        },
+        series: [{
+          name: ' ',
+          data: currentScenario.companyValueDistribution.yValues,
+        }],
+      });
+    });
   }
 
   ngOnDestroy() {
@@ -189,26 +237,27 @@ export class ScenarioDetailComponent implements OnInit, OnDestroy {
       const base = this.accountingDataFormGroup.controls.base.value;
       const end = this.accountingDataFormGroup.controls.end.value;
       Object.keys(paramData)
-      .filter(param => [undefined, this.accountingDataFormGroup.value.calculateFcf].indexOf(this.paramData[param].showOnCalculation) > -1)
-      .forEach((param) => {
-        const paramFormGroup = this.accountingDataFormGroup.controls[param];
-        if (paramFormGroup.value.isHistoric && !currentScenario.stochastic) {
-          currentScenario.stochastic = true;
-        }
-        currentScenario[param] = {
-          isHistoric: paramFormGroup.value.isHistoric,
-          timeSeries: paramFormGroup.value.timeSeries.filter(dataPoint =>
-            this.isInsideBounds(quarterly, start, end, dataPoint)
-            && this.checkVisibility(dataPoint, paramFormGroup.value.isHistoric, quarterly, base, end, paramData[param].shiftDeterministic)),
-        };
-      });
+        .filter(param => [undefined, this.accountingDataFormGroup.value.calculateFcf].indexOf(this.paramData[param].showOnCalculation) > -1)
+        .forEach((param) => {
+          const paramFormGroup = this.accountingDataFormGroup.controls[param];
+          if (paramFormGroup.value.isHistoric && !currentScenario.stochastic) {
+            currentScenario.stochastic = true;
+          }
+          currentScenario[param] = {
+            isHistoric: paramFormGroup.value.isHistoric,
+            timeSeries: paramFormGroup.value.timeSeries.filter(dataPoint =>
+              this.isInsideBounds(quarterly, start, end, dataPoint) &&
+              this.checkVisibility(dataPoint, paramFormGroup.value.isHistoric, quarterly, base, end, paramData[param].shiftDeterministic)),
+          };
+        });
 
       this._scenariosService.updateScenario(currentScenario).subscribe(
         () => {
           this.editable = false;
           this.formGroup.disable();
+          this._alertService.success('Scenario wurde gespeichert');
         },
-        () => console.log('ERROR'), // TODO error handling
+        () => this._alertService.warning('Scenario konnte nicht gespeichert werden'), // TODO error handling
       );
     });
   }
@@ -222,9 +271,11 @@ export class ScenarioDetailComponent implements OnInit, OnDestroy {
 
       this._optionsService.setConfig(config);
     });
+
+    this._alertService.success('Konfiguration wurde gespeichert');
   }
 
-  setEditable(editable: Boolean, save?: Boolean) {
+  setEditable(editable: Boolean, save ?: Boolean) {
     if (editable) {
       this.formGroup.enable();
       this.editable = editable;
@@ -233,7 +284,7 @@ export class ScenarioDetailComponent implements OnInit, OnDestroy {
         if (this.formGroup.valid && this.accountingDataFormGroup.valid) {
           this.saveScenario();
         } else {
-          console.log(this.formGroup, this.accountingDataFormGroup); // TODO snackbar
+          this._alertService.error('Speichern des Scenarios nicht möglich. Bitte alle benötigten Felder ausfüllen');
         }
       } else {
         this.editable = editable;
@@ -250,14 +301,14 @@ export class ScenarioDetailComponent implements OnInit, OnDestroy {
 
   checkValue(value, requireHistoric: Boolean, quarterly: Boolean, base, shifted = false) {
     return ((value.year < base.year) || (value.year === base.year &&
-      (!quarterly || value.quarter <= base.quarter))) === requireHistoric
-      || (shifted && value.year === base.year && (!quarterly || value.quarter === base.quarter));
+        (!quarterly || value.quarter <= base.quarter))) === requireHistoric ||
+      (shifted && value.year === base.year && (!quarterly || value.quarter === base.quarter));
   }
 
   isInsideBounds(quarterly, start, end, value) {
     return (value.year > start.year - (quarterly ? 0 : 1) ||
-      (quarterly && value.year === start.year
-        && value.quarter >= start.quarter)) &&
+        (quarterly && value.year === start.year &&
+          value.quarter >= start.quarter)) &&
       (value.year < end.year + (quarterly ? 0 : 1) ||
         (quarterly && value.year === end.year && value.quarter <= end.quarter));
   }
