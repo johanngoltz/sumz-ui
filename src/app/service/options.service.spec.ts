85a9bf5ca69835d893c69ec7b48f50817702c8db
Johann Goltz <johann-goltz@hotmail.de>
2018-07-03 11:09:50 +0200

    fix OptionsService tests

--- src/app/service/options.service.spec.ts
+++ src/app/service/options.service.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { OptionsService } from './options.service';
-import { RemoteConfig } from '../api/config';
+import { RemoteConfig, ScenarioConfig } from '../api/config';
 import { skip } from 'rxjs/operators';
 
 describe('OptionsService', () => {
@@ -18,16 +18,15 @@ describe('OptionsService', () => {
   it('should return the remote configuration', inject([OptionsService], (service: OptionsService) => {
     service.getConfig().subscribe(config => {
       expect(config).toBeTruthy();
-      expect(config.showResult).toBeDefined();
-      expect(config.showResult.fte).toBeDefined();
+      expect(config.scenarioConfig).toBeDefined();
+      expect(config.scenarioConfig.get(1).showResult.fte).toBeDefined();
     }, fail);
   }));
 
   it('should return the remote configuration', inject([OptionsService], (service: OptionsService) => {
     const newConfig: RemoteConfig = {
-      showResult: {
-        apv: true, fte: false, cvd: true, fcf: true,
-      },
+      scenarioConfig: new Map<number, ScenarioConfig>([
+        [1, { showResult: { apv: true, cvd: true, fcf: false, fte: false } }]]),
     };
     service.getConfig().subscribe();
     service.setConfig(newConfig).pipe(skip(1)).subscribe(updatedConfig => {
