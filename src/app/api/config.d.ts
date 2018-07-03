e244cb6724ec0dd9ee66250927a4be7f307851a7
Johann Goltz <johann-goltz@hotmail.de>
2018-07-03 11:06:53 +0200

    save results to be displayed per scenario

--- src/app/api/config.d.ts
+++ src/app/api/config.d.ts
@@ -1,8 +1,12 @@
 export interface RemoteConfig {
+    scenarioConfig: Map<number, ScenarioConfig>;
+}
+
+interface ScenarioConfig {
     showResult: {
         cvd: boolean,
         apv: boolean,
         fcf: boolean,
         fte: boolean
-    };
+    }
 }
