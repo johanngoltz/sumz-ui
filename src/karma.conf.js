cadb5dded2438fc23755d981cca9b7bc7027760f
Sta7e <jonas-robl@web.de>
2018-07-05 01:15:57 +0200

    Anmerkungen von Johann

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
     customLaunchers: {
       ChromeDebug: {
         base: 'Chrome',
-        flags: ['--headless', '--disable-gpu', '--remote-debugging-port=9222'],
+        flags: ['--remote-debugging-port=9222'],
         debug: true
       },
       FirefoxHeadless: {
