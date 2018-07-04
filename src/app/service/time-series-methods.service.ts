d15fd818c06a2d0432471491f4929216eceb30da
Johann Goltz <j.goltz@sap.com>
2018-07-04 13:27:05 +0200

    refactor TimeSeriesMethodsService.isInsideBounds

--- src/app/service/time-series-methods.service.ts
+++ src/app/service/time-series-methods.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { DataPoint } from '../api/scenario';
 
 @Injectable({
   providedIn: 'root',
@@ -7,23 +8,32 @@ export class TimeSeriesMethodsService {
 
   constructor() { }
 
-  checkVisibility(value, requireHistoric: Boolean, quarterly: Boolean, base, end, shifted = false) {
+  checkVisibility(value: DataPoint, requireHistoric: Boolean, quarterly: Boolean, base: TimePoint, end: TimePoint, shifted = false) {
     return this.checkValue(value, requireHistoric, quarterly, base, shifted) &&
       (!shifted || value.year !== end.year || (quarterly && value.quarter !== end.quarter));
   }
 
-  checkValue(value, requireHistoric: Boolean, quarterly: Boolean, base, shifted = false) {
+  checkValue(value: DataPoint, requireHistoric: Boolean, quarterly: Boolean, base: TimePoint, shifted = false) {
     return ((value.year < base.year) || (value.year === base.year &&
       (!quarterly || value.quarter <= base.quarter))) === requireHistoric
       || (shifted && value.year === base.year && (!quarterly || value.quarter === base.quarter));
   }
 
-  isInsideBounds(quarterly, start, end, value) {
-    return (value.year > start.year - (quarterly ? 0 : 1) ||
-      (quarterly && value.year === start.year
-        && value.quarter >= start.quarter)) &&
-      (value.year < end.year + (quarterly ? 0 : 1) ||
-        (quarterly && value.year === end.year && value.quarter <= end.quarter));
+  isInsideBounds(quarterly: Boolean, start: TimePoint, end: TimePoint, value: TimePoint) {
+    if (!quarterly) {
+      // Kopien anlegen und quarter-Property löschen. "delete" geht nicht wg. call-by-reference.
+      [start, end, value] = [start, end, value].map(point => ({ year: point.year }));
+    }
+    return !this.isBefore(value, start) && !this.isBefore(end, value);
   }
 
+  private isBefore(first: TimePoint, second: TimePoint) {
+    // Wichtig zu wissen: (undefined < undefined) === false
+    return first.year < second.year || (first.year === second.year && first.quarter < second.quarter);
+  }
+}
+
+interface TimePoint {
+  year: number;
+  quarter?: number;
 }
