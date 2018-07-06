54d384474c1eec2f2640d4a3a89f181a32c3801b
Johann Goltz <johann-goltz@hotmail.de>
2018-07-06 11:31:26 +0200

    accountingDataParams: Map instead of abstract class

--- src/app/api/paramData.ts
+++ src/app/api/paramData.ts
@@ -1,4 +1,4 @@
-export abstract class AccountingDataParams {
+/*export abstract class AccountingDataParams {
     revenue: { displayName: 'Umsatzerlöse', showOnCalculation: true };
     additionalIncome: { displayName: 'Sonstige Erlöse', showOnCalculation: true };
     costOfMaterial: { displayName: 'Materialkosten', showOnCalculation: true };
@@ -8,7 +8,24 @@ export abstract class AccountingDataParams {
     divestments: { displayName: 'Desinvestitionen', showOnCalculation: true };
     freeCashFlows: { displayName: 'Free Cash Flow', showOnCalculation: false };
     liabilities: { displayName: 'Fremdkapital', showOnCalculation: undefined, shiftDeterministic: true };
-}
+}*/
+
+export type AccountingDataParams = ReadonlyMap<string, AccountingDataParam>;
+
+export const accountingDataParams: AccountingDataParams =
+    new Map([
+        ['revenue', { displayName: 'Umsatzerlöse', showOnCalculation: true }],
+        ['additionalIncome', { displayName: 'Sonstige Erlöse', showOnCalculation: true }],
+        ['costOfMaterial', { displayName: 'Materialkosten', showOnCalculation: true }],
+        ['costOfStaff', { displayName: 'Personalkosten', showOnCalculation: true }],
+        ['additionalCosts', { displayName: 'Sonstige Kosten', showOnCalculation: true }],
+        ['investments', { displayName: 'Investitionen', showOnCalculation: true }],
+        ['divestments', { displayName: 'Desinvestitionen', showOnCalculation: true }],
+        ['freeCashFlows', { displayName: 'Free Cash Flow', showOnCalculation: false }],
+        ['liabilities', { displayName: 'Fremdkapital', showOnCalculation: undefined, shiftDeterministic: true }],
+    ]);
+
+export interface AccountingDataParam { displayName: string; showOnCalculation?: boolean; shiftDeterministic?: boolean; }
 
 export const environmentParams = {
     equityInterestRate: {},
